16ecaf437803c940b1e974153c211320af085a1a
HectorOtero <125500048+HectorOtero@users.noreply.github.com>
2025-05-03 22:01:56 +0200

    Primera parte Lobby

--- Parchis/Source.cpp
+++ Parchis/Source.cpp
@@ -1,22 +1,59 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <string>
+
+#define LISTENER_PORT 55000
+
 
 void main ()
 {
-	sf::RenderWindow window(sf::VideoMode({ 200,200 }), "SFML works");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::TcpListener listener;
+	sf::TcpSocket Client;
+	sf::Packet receivedPacket;
+	std::string codigoSala;
 
-	while (window.isOpen())
-	{
-		while (const std::optional event = window.pollEvent())
+	bool closeServer = false;
+
+
+	if (listener.listen(LISTENER_PORT) != sf::Socket::Status::Done) {
+		std::cerr << "Error al escuchar el puerto" << LISTENER_PORT << std::endl;
+		return;
+	}
+
+		std::cout << "Esperando conexion..." << LISTENER_PORT << std::endl;
+
+		if (listener.accept(Client) != sf::Socket::Status::Done) {
+			std::cerr << "Error al aceptar conexión del cliente" << std::endl;
+			return;
+		}
+		
+		std::cout << "Cliente conectado desde: " << Client.getRemoteAddress().value() << std::endl;
+
+		while (true)
 		{
-			if (event->is<sf::Event::Closed>())
+			sf::Packet packet;
+			sf::Socket::Status status = Client.receive(packet);
+
+			if (status == sf::Socket::Status::Disconnected)
 			{
-				window.close();
+				std::cout << "Cliente desconectado" << std::endl;
+				break;
+			}
+			else if (status != sf::Socket::Status::Done)
+			{
+				std::cerr << "Error al recibir el paquete" << std::endl;
+				break;
+			}
+
+			std::string codigoSala;
+			if (!(packet >> codigoSala))
+			{
+				std::cerr << "Error al extraer el codigo de sala del paquete" << std::endl;
+				break;
+			}
+			else {
+				std::cout << "Codigo de sala recibido: " << codigoSala << std::endl;
 			}
 		}
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+
 }
